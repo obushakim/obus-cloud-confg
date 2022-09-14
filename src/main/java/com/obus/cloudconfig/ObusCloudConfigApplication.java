f3b92c1f5c79eb226134c15e6025cb3255aeb27e
obushakim <obushakim@gmail.com>
2022-09-14 16:01:15 +0700

    test

--- src/main/java/com/obus/cloudconfig/ObusCloudConfigApplication.java
+++ src/main/java/com/obus/cloudconfig/ObusCloudConfigApplication.java
@@ -3,7 +3,10 @@ package com.obus.cloudconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableConfigServer
 @SpringBootApplication
 public class ObusCloudConfigApplication {
@@ -12,4 +15,8 @@ public class ObusCloudConfigApplication {
 		SpringApplication.run(ObusCloudConfigApplication.class, args);
 	}
 
+	@GetMapping
+	public String index() {
+		return "obus cloud config index";
+	}
 }
